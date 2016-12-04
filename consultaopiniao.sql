42972665e2bfb973e61d16d98c00d6aa16032243
edsonjunioo <edsonjunio.sousa@gmail.com>
2016-12-04 21:54:35 -0200

    consultaopiniao
    todas as tabelas criadas, sem as chaves e restrições - 21:54hs

--- consultaopiniao.sql
+++ consultaopiniao.sql
@@ -33,7 +33,7 @@ SET default_with_oids = false;
 
 
 
-
+--TABELA PESSOA
 CREATE TABLE pessoa (
 	nome character varying(250) NOT NULL,
 	cpf character(11) NOT NULL,
@@ -45,6 +45,7 @@ CREATE TABLE pessoa (
 ALTER TABLE pessoa OWNER TO postgres;
 
 
+--TABELA PROFESSOR
 CREATE TABLE professor (
 	siape character(11) NOT NULL,
 	un_academica character varying(20) NOT NULL,
@@ -53,22 +54,94 @@ CREATE TABLE professor (
 
 ALTER TABLE professor OWNER TO postgres;
 
-CREATE TABLE aluno(
-	nro_matricula character(11) NOT NULL
-	
-	
 
+--TABELA ALUNO
+CREATE TABLE aluno(
+	nro_matricula character(11) NOT NULL,
+	curso character varying(20) NOT NULL
 );
 
 ALTER TABLE aluno OWNER TO postgres;
 
 
 
+--TABELA TECNICO
+CREATE TABLE tecnico(
+	siape character(11) NOT NULL,
+	un_adm character varying(20) NOT NULL
+);
+
+ALTER TABLE tecnico OWNER TO postgres;
 
 
-	
-	
 
+--TABELA TERCEIRIZADO
+CREATE TABLE terceirizado(
+	empresa character varying(20) NOT NULL,
+	setor character varying(20) NOT NULL
+);
+
+ALTER TABLE terceirizado OWNER TO postgres;	
+
+
+
+--TABELA UNIDADE ACADEMICA
+CREATE TABLE unidade_acadêmica(
+	sigla character(5) NOT NULL,
+	nome character varying(150) NOT NULL,
+	area character varying(20) NOT NULL
+
+);
+
+ALTER TABLE unidade_acadêmica OWNER TO postgres;
+
+
+
+--TABELA CURSO	
+CREATE TABLE curso(
+	sigla character(5) NOT NULL,
+	nome character varying(150) NOT NULL,
+	un_acad character(5) NOT NULL,
+);
+
+ALTER TABLE curso OWNER TO postgres;
+
+
+--TABELA QUESTÃO
+CREATE TABLE questao(
+	descricao character varying(300) NOT NULL,
+	id_questao character(10) NOT NULL,
+	resposta character varying(300) NOT NULL
+
+);
+
+ALTER TABLE questao OWNER TO postgres;	
+
+
+
+--TABELA RESPOSTA
+CREATE TABLE resposta(
+	resp character varying(300) NOT NULL,
+	id_resp character(10) NOT NULL
+
+);
+
+ALTER TABLE resposta OWNER TO postgres;
+
+
+
+--TABELA FORMULARIO
+CREATE TABLE formulario(
+	id_form character(10) NOT NULL,
+	nome character varying(60) NOT NULL,
+	criador character varying(100) NOT NULL,
+	data_inicial date NOT NULL,
+	data_final date NOT NULL
+
+
+);
+
+ALTER TABLE formulario OWNER TO postgres;
 
 
 
